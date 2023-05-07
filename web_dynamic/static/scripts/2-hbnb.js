3d53e3c956b1aae28fcaf6cbfccc89ccb8e33627
Olamide <olire96@gmail.com>
2023-05-07 09:44:12 +0100

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,4 +14,15 @@ $(document).ready(function(){
         const checkedItems = Object.values(clickedAmenities).join(", ");
         $(".amenities h4").text(checkedItems);
     });
+    $.get("http://localhost:5001/api/v1/status/", function(data){
+        //if the status is okay, add the class available to api_status
+        console.log(data.status);
+        if (data.status === 'OK'){
+            $("#api_status").addClass("available");
+        }
+        //else remove the class from api_status
+        else{
+            $("#api_staus").removeClass("available");
+        }
+    });
 });
